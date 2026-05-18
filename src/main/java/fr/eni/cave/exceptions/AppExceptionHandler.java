5444451e93ef14ce636d6a9b3a1d104bc9c6406c
Quentin PRIGENT <qprigent@campus-eni.fr>
2026-05-18 13:48:46 +0200

    Review

--- src/main/java/fr/eni/cave/exceptions/AppExceptionHandler.java
+++ src/main/java/fr/eni/cave/exceptions/AppExceptionHandler.java
@@ -4,11 +4,15 @@ import lombok.AllArgsConstructor;
 import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import fr.eni.cave.dto.ResponseApi;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 @ControllerAdvice
@@ -17,13 +21,13 @@ public class AppExceptionHandler {
 
     private MessageSource messageSource;
 
-    @ExceptionHandler(value = { HttpRequestMethodNotSupportedException.class })
-    public ResponseEntity<ResponseApi<String>> methodNotAllowedException(
-            HttpRequestMethodNotSupportedException Exception,
+    @ExceptionHandler(value = { Exception.class })
+    public ResponseEntity<ResponseApi<String>> allException(
+            Exception exception,
             Locale locale
     ) {
         String errorMessage = messageSource.getMessage(
-                "validation.requests.httpRequestMethodNotSupportedException",
+                "notvalidexception",
                 null,
                 locale
         );
@@ -34,7 +38,34 @@ public class AppExceptionHandler {
                         ResponseApi.<String>builder()
                                 .statusCode(HttpStatus.NOT_ACCEPTABLE.value())
                                 .message(HttpStatus.NOT_ACCEPTABLE.name())
-                                .data(errorMessage)
+                                .data(errorMessage + exception.getMessage())
+                                .build()
+                );
+    }
+
+    @ExceptionHandler(value = { MethodArgumentNotValidException.class })
+    public ResponseEntity<ResponseApi<List<String>>> methodArgumentNotValidException(
+            MethodArgumentNotValidException exception,
+            Locale locale
+    ) {
+        String errorMessage = messageSource.getMessage(
+                "notvalidexception",
+                null,
+                locale
+        );
+
+        List<String> messages = new ArrayList<>();
+        for (FieldError error : exception.getFieldErrors()) {
+            messages.add(error.getDefaultMessage());
+        }
+
+        return ResponseEntity
+                .status(HttpStatus.NOT_ACCEPTABLE)
+                .body(
+                        ResponseApi.<List<String>>builder()
+                                .statusCode(HttpStatus.NOT_ACCEPTABLE.value())
+                                .message(HttpStatus.NOT_ACCEPTABLE.name())
+                                .data(messages)
                                 .build()
                 );
     }
